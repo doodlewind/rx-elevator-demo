2c22d714857193669ed6c9a6a7f698f1a087c2f8
Yifeng Wang <i@ewind.us>
2017-07-22 21:31:33 +0800

    add target / curr comparison

--- src/rx/index.js
+++ src/rx/index.js
@@ -4,14 +4,21 @@ export function getStream (emitter, type) {
   return Observable
     .fromEvent(emitter, type)
     .switchMap(({ target, curr }) => {
-      const up = Observable
-        .interval(1000)
-        .map(x => x + curr)
-        .take(target + 1 - curr)
-      const down = Observable
-        .interval(1000)
-        .map(x => target - x)
-        .take(target)
-      return up.concat(down)
+      if (target >= curr) {
+        const up = Observable
+          .interval(1000)
+          .map(x => x + curr)
+          .take(target + 1 - curr)
+        const down = Observable
+          .interval(1000)
+          .map(x => target - x)
+          .take(target)
+        return up.concat(down)
+      } else {
+        return Observable
+          .interval(1000)
+          .map(x => curr - x)
+          .take(curr)
+      }
     })
 }
