f10d79bc9f4f098030e06709a50ce6d5448ffc0e
Yifeng Wang <i@ewind.us>
2017-08-25 20:04:27 +0800

    record selected floor state

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
-    <div v-for="i in (0, 10)" :key="i">
-      <span>{{11 - i}}</span>
-      <button @click="command('down', 11 - i)">↓</button>
+    <div v-for="(floor, i) in floors" :key="i">
+      <span>{{floors.length - i}}</span>
+      <button @click="command('down', floors.length + 1 - i, i)">↓</button>
+      <span :style="{color: floor.up ? 'red' : 'white'}">↑</span>
+      <span :style="{color: floor.down ? 'red' : 'white'}">↓</span>
     </div>
     <hr/>
-    <div>F{{currentFloor}}</div>
+    <div>F{{curr.floor}} - {{curr.state}}</div>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
 import EventEmitter from 'events'
 import { getStream } from './rx'
 const emitter = new EventEmitter()
@@ -17,17 +20,37 @@ const emitter = new EventEmitter()
 export default {
   created () {
     const stream = getStream(emitter, 'click')
-    stream.subscribe(val => this.currentFloor = val)
+    stream.subscribe(val => this.curr.floor = val)
   },
   data () {
-    return { currentFloor: 1 }
+    return {
+      floors: [
+        { up: false, down: false },
+        { up: false, down: false },
+        { up: false, down: false },
+        { up: false, down: false },
+        { up: false, down: false },
+        { up: false, down: false },
+        { up: false, down: false },
+        { up: false, down: false },
+        { up: false, down: false },
+        { up: false, down: false }
+      ],
+      curr: {
+        floor: 1,
+        state: 'stop'
+      }
+    }
   },
   methods: {
-    command (direction, target) {
+    command (direction, targetFloor, index) {
+      const currFloor = this.floors[index]
+      Vue.set(currFloor, direction, !currFloor[direction])
+      // console.log(direction, floor)
       emitter.emit('click', {
         direction,
-        target,
-        curr: this.currentFloor
+        targetFloor,
+        currFloor: this.curr.floor
       })
     }
   }
--- src/rx/index.js
+++ src/rx/index.js
@@ -3,22 +3,22 @@ import { Observable } from 'rxjs'
 export function getStream (emitter, type) {
   return Observable
     .fromEvent(emitter, type)
-    .switchMap(({ target, curr }) => {
-      if (target >= curr) {
+    .switchMap(({ targetFloor, currFloor }) => {
+      if (targetFloor >= currFloor) {
         const up = Observable
           .interval(1000)
-          .map(x => x + curr)
-          .take(target + 1 - curr)
+          .map(x => x + currFloor)
+          .take(targetFloor + 1 - currFloor)
         const down = Observable
           .interval(1000)
-          .map(x => target - x)
-          .take(target)
+          .map(x => targetFloor - x)
+          .take(targetFloor)
         return up.concat(down)
       } else {
         return Observable
           .interval(1000)
-          .map(x => curr - x)
-          .take(curr)
+          .map(x => currFloor - x)
+          .take(currFloor)
       }
     })
 }
