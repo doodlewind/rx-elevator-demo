3669d325e1cfa3027655144411d96f996509c79b
Yifeng Wang <i@ewind.us>
2017-08-27 11:06:13 +0800

    feat: compute max target floor

--- src/rx/index.js
+++ src/rx/index.js
@@ -1,46 +1,42 @@
 import { Observable } from 'rxjs'
 
+function getMaxTargetFloor (floors) {
+  let maxTargetFloor = 0
+  for (let i = floors.length - 1; i >= 0; i--) {
+    if (floors[i].down) maxTargetFloor = floors.length - i
+  }
+  return maxTargetFloor
+}
+
 export function getStream (emitter, type) {
   return Observable
     .fromEvent(emitter, type)
+    .filter(({ floors, targetFloor, currFloor, currDirection }) => {
+      if (currDirection === 'down') return false
+      else if (currDirection === 'up' && targetFloor <= currFloor) {
+        return false
+      } else return true
+    })
     .switchMap(({ floors, targetFloor, currFloor, currDirection }) => {
-      console.log(floors)
-      console.log(currFloor, currDirection)
+      const maxTargetFloor = getMaxTargetFloor(floors)
       const baseFloor = currFloor
-
-      if (targetFloor >= baseFloor) {
-        const up = Observable
-          .interval(1000)
-          .map(count => {
-            const newFloor = count + baseFloor
-            return {
-              floor: newFloor,
-              direction: newFloor === targetFloor ? 'stop' : 'up'
-            }
-          })
-          .take(targetFloor + 1 - baseFloor)
-        const down = Observable
-          .interval(1000)
-          .map(count => {
-            const newFloor = targetFloor - count
-            return {
-              floor: newFloor,
-              direction: newFloor !== 1 ? 'down' : 'stop'
-            }
-          })
-          .take(targetFloor)
-        return up.concat(down)
-      } else {
-        return Observable
-          .interval(1000)
-          .map(count => {
-            const newFloor = baseFloor - count
-            return {
-              floor: newFloor,
-              direction: newFloor !== 1 ? 'down' : 'stop'
-            }
-          })
-          .take(baseFloor)
-      }
+      const up = Observable
+        .interval(1000)
+        .map(count => {
+          const newFloor = count + baseFloor
+          return { floor: newFloor, direction: 'up' }
+        })
+        .take(maxTargetFloor + 1 - baseFloor)
+      const down = Observable
+        .interval(1000)
+        .map(count => {
+          const newFloor = maxTargetFloor - count
+          return {
+            floor: newFloor,
+            direction: newFloor !== 1 ? 'down' : 'stop'
+          }
+        })
+        .take(maxTargetFloor)
+      return up.concat(down)
     })
 }
